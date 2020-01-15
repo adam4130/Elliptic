412b8ffcb9bc3c7c1cef74a24c64fd383be24070
Adam Lee <adam4130@users.noreply.github.com>
2020-01-15 15:27:46 +0100

    Moved table used in BabyGiant algorithm to separate function

--- include/babygiant.h
+++ include/babygiant.h
@@ -1,22 +1,23 @@
 #ifndef BABYGIANT_H
 #define BABYGIANT_H
 
+#include <unordered_map> // std::unordered_map
+
 #include "curve.h"
 
 namespace elliptic {
 
     class BabyGiant {
+    public:
+        BabyGiant(Curve* curve) : curve_(curve) {}
+        mpz_class discreteLogarithm(const Point& P, const Point& G) const;
     private:
         static const long MEMORY_LIMIT;
-
         Curve* curve_;
 
         mpz_class getRandom(mpz_class n) const;
-
-    public:
-        BabyGiant(Curve* curve) : curve_(curve) {}
-
-        mpz_class discreteLogarithm(const Point& P, const Point& G) const;
+        void populateTable(std::unordered_map<Point, long, PointHasher>& table,
+            const Point& G, mpz_class m) const;
     };
 
 }
--- src/babygiant.cpp
+++ src/babygiant.cpp
@@ -3,13 +3,68 @@
 #include <cstdlib>       // std::rand, std::srand
 #include <ctime>         // std::time
 #include <stdexcept>     // std::invalid_argument
-#include <unordered_map> // std::unordered_map
 
 const long elliptic::BabyGiant::MEMORY_LIMIT = 50000000;
 
 /**
+ * Computes a discrete logarithm on the given elliptic curve i.e., finds k such
+ * that kG = P. The basic algorithm has space and time complexity of O(\sqrt{n})
+ * where n is the order of the curve. Completeness is not guaranteed for \sqrt{n}
+ * greater than the memory limit.
+ */
+mpz_class elliptic::BabyGiant::discreteLogarithm(const Point& P, const Point& G) const {
+    if (!curve_->hasPoint(P) || !curve_->hasPoint(G)) {
+        throw std::invalid_argument("Public key or base point is not on the curve");
+    }
+
+    mpz_class n = curve_->getOrder();
+    mpz_class m = sqrt(n) + 1; // m = ceil(sqrt(n))
+
+    std::unordered_map<Point, long, PointHasher> table;
+    populateTable(table, G, m);
+
+    mpz_class r = getRandom(m);
+    Point mG = curve_->multiply(G, m);
+    Point imG = curve_->multiply(mG, r);
+    for (mpz_class i = r; i <= m; i++) {
+        // Q = P - imG
+        Point Q = curve_->add(P, curve_->negatePoint(imG));
+
+        if (table.find(Q) != table.end()) {
+            long j = table[Q];
+            mpz_class k = i*m + j;
+            mpz_mod(k.get_mpz_t(), k.get_mpz_t(), n.get_mpz_t());
+            return k;
+        }
+
+        imG = curve_->add(imG, mG);
+    }
+
+    throw std::invalid_argument("Could not find k such that kG = P, increase memory limit");
+}
+
+/**
+ * Fills the given hash map with points, { jG | 1 <= j <= min(m, MEMORY_LIMIT) }, mapping
+ * to their multiplier, j.
+ */
+void elliptic::BabyGiant::populateTable(std::unordered_map<Point, long, PointHasher>& table,
+        const Point& G, mpz_class m) const {
+    long size = MEMORY_LIMIT;
+    if (cmp(MEMORY_LIMIT, m) > 0) {
+        size = m.get_si();
+    }
+
+    Point jG;
+    for (long j = 1; j <= size; j++) {
+        jG = curve_->add(jG, G);
+        table[jG] = j;
+    }
+}
+
+/**
  * Get a uniform random number in 0 to n - 1, inclusive. Seeding is done by the
- * system time.
+ * system time. This method should NOT be used to generate random numbers for
+ * private keys (consider using `getRandom` in "hash.h" which relies on /dev/random).
  */
 mpz_class elliptic::BabyGiant::getRandom(mpz_class n) const {
     gmp_randstate_t state;
@@ -26,48 +81,3 @@ mpz_class elliptic::BabyGiant::getRandom(mpz_class n) const {
     return random;
 }
 
-/**
- * Computes a discrete logarithm on the given elliptic curve i.e., finds k such
- * that kG = P. The basic algorithm has space and time complexity of O(\sqrt{n})
- * where n is the order of the curve. Completeness is not guaranteed for \sqrt{n}
- * greater than the memory limit.
- */
-mpz_class elliptic::BabyGiant::discreteLogarithm(const Point& P, const Point& G) const {
-    if (!curve_->hasPoint(P) || !curve_->hasPoint(G)) {
-        throw std::invalid_argument("Public key or base point is not on the curve");
-    }
-
-    mpz_class n = curve_->getOrder();
-    mpz_class m = sqrt(n) + 1; // m = ceil(sqrt(n))
-
-    long size = MEMORY_LIMIT;
-    if (cmp(MEMORY_LIMIT, m) > 0) {
-        size = m.get_si();
-    }
-
-    Point jG;
-    std::unordered_map<Point, long, PointHasher> map;
-    for (long j = 1; j <= size; j++) {
-        jG = curve_->add(jG, G);
-        map[jG] = j;
-    }
-
-    mpz_class r = getRandom(m);
-    Point mG = curve_->multiply(G, m);
-    Point imG = curve_->multiply(mG, r);
-    for (mpz_class i = r; i <= m; i++) {
-        // Q = P - imG
-        Point Q = curve_->add(P, curve_->negatePoint(imG));
-
-        if (map.find(Q) != map.end()) {
-            long j = map[Q];
-            mpz_class k = i*m + j;
-            mpz_mod(k.get_mpz_t(), k.get_mpz_t(), n.get_mpz_t());
-            return k;
-        }
-
-        imG = curve_->add(imG, mG);
-    }
-
-    throw std::invalid_argument("Could not find k such that kG = P, increase memory limit");
-}
